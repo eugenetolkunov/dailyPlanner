95ec38fdaf3d9423d2ccefdb543737e802452862
Yevhen Tolkunov <yevhentolkunov@gmail.com>
2023-03-16 16:04:41 +0200

    Add daily planner

--- src/main/java/mate/academy/DailyPlanner.java
+++ src/main/java/mate/academy/DailyPlanner.java
@@ -1,8 +1,17 @@
 package mate.academy;
 
 public class DailyPlanner {
+    public static void main(String[] args) {
+        String[] pl = {"wake up", "have breakfast", "go to work", "solve 5 tasks from codewars"};
+        System.out.println(createDailyPlan(pl));
+    }
     public static String createDailyPlan(String[] plans) {
-        return
+        StringBuilder builder = new StringBuilder();
+        builder.append("My plans: \n");
+        for(int i = 0; i < plans.length; i++) {
+            builder.append(i + 1 + ".) ").append(plans[i]).append("; \n");
+        }
+        return new String(builder);
     }
 
 }
